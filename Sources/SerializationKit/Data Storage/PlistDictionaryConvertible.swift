5512db33bd530e2026bfeeecd3af0cc68cf753f8
Ryan Boyer <ryanjboyer@gmail.com>
2023-02-23 18:16:29 -0700

    Backup

--- Sources/SerializationKit/Data Storage/PlistDictionaryConvertible.swift
+++ Sources/SerializationKit/Data Storage/PlistDictionaryConvertible.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// A `SerializedDictionaryConvertible` with `plist` data conversion.
 public protocol PlistDictionaryConvertible: SerializedDictionaryConvertible, SerializedDataConvertible {
-	var format: PropertyListSerialization.PropertyListFormat { get }
+	static var format: PropertyListSerialization.PropertyListFormat { get }
 }
 
 public extension PlistDictionaryConvertible {
@@ -16,7 +16,7 @@ public extension PlistDictionaryConvertible {
 	}
 
 	func toData() throws -> Data {
-		try toData(format: format)
+		try toData(format: Self.format)
 	}
 
 	func toData(format: PropertyListSerialization.PropertyListFormat) throws -> Data {
---
+++ Sources/SerializationKit/Extensions/ClosedRange+Extension.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+extension ClosedRange: SerializedDictionaryConvertible & Serializable where Bound: SerializedObjectConvertible {
+	public init(dictionary: [String: Any]) {
+		let lowerBound: Bound! = Bound(unwrap: dictionary["lowerBound"])
+		let upperBound: Bound! = Bound(unwrap: dictionary["upperBound"])
+		self.init(uncheckedBounds: (lowerBound, upperBound))
+	}
+
+	public func toDictionary() -> [String: Any?] {
+		[
+			"lowerBound": lowerBound,
+			"upperBound": upperBound,
+		]
+	}
+}
