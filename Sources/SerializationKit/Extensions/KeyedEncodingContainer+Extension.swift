de6619321aa3f368d0f627d17eaa6c16a5f040a9
Ryan Boyer <ryanjboyer@gmail.com>
2023-09-25 10:03:08 -0600

    Update KeyedEncodingContainer+Extension.swift

--- Sources/SerializationKit/Extensions/KeyedEncodingContainer+Extension.swift
+++ Sources/SerializationKit/Extensions/KeyedEncodingContainer+Extension.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public extension KeyedEncodingContainer {
-	mutating func encode<T: CodableMetatypeAccessor, U: Codable>(_ value: U, using: T.Type, forKey key: KeyedEncodingContainer<K>.Key) throws {
+	mutating func encode<T: CodableMetatypeAccessor, U: Codable>(_ value: U, using metatype: T.Type, forKey key: KeyedEncodingContainer<K>.Key) throws {
 		let valueType = type(of: value)
 		guard let base = T.allCases.first(where: { metatype in metatype.codableMetatype == valueType }) else {
 			print("Failed to find a valid metatype in \(T.self) for the given value \(value)")
@@ -11,7 +11,7 @@ public extension KeyedEncodingContainer {
 	}
 
 	mutating func encode<T: CodableMetatypeAccessor>(_ values: [any Codable], using metatype: T.Type, forKey key: KeyedEncodingContainer<K>.Key) throws {
-		try encode(values.compactMap { (value) -> CodablePayload<T>? in
+		try encode(values.compactMap { value -> CodablePayload<T>? in
 			let valueType = type(of: value)
 			guard let base = T.allCases.first(where: { metatype in metatype.codableMetatype == valueType }) else {
 				print("Failed to find a valid metatype in \(T.self) for the given value \(value)")
@@ -19,7 +19,20 @@ public extension KeyedEncodingContainer {
 			}
 			return CodablePayload(value, base: base)
 		}, forKey: key)
+	}
 
+	mutating func encodeIfPresent<T: CodableMetatypeAccessor, U: Codable>(_ value: U?, using metatype: T.Type, forKey key: KeyedEncodingContainer<K>.Key) throws {
+		guard let value else {
+			return
+		}
+		try encode(value, using: metatype, forKey: key)
+	}
+
+	mutating func encodeIfPresent<T: CodableMetatypeAccessor>(_ values: [any Codable]?, using metatype: T.Type, forKey key: KeyedEncodingContainer<K>.Key) throws {
+		guard let values else {
+			return
+		}
+		try encode(values, using: metatype, forKey: key)
 	}
 
 //	mutating func encode(_ values: [any Encodable], forKey key: KeyedEncodingContainer<K>.Key) throws {
