e4eba39a7cce674e533880529a54aa8b99b7b976
Ryan Boyer <ryanjboyer@gmail.com>
2023-12-24 09:32:57 -0700

    Update SerializedFileConvertible.swift

--- Sources/SerializationKit/Protocols/SerializedFileConvertible.swift
+++ Sources/SerializationKit/Protocols/SerializedFileConvertible.swift
@@ -3,13 +3,13 @@ import Foundation
 // MARK: - SerializedFileReadable
 
 public protocol SerializedFileReadable: SerializedDataReadable {
-	init?(url: URL)
+	init(url: URL) throws
 }
 
 // MARK: - SerializedFileWritable
 
 public protocol SerializedFileWritable: SerializedDataWritable {
-	func save(to url: URL)
+	func save(to url: URL) throws
 }
 
 // MARK: - Typealias
@@ -19,34 +19,25 @@ public typealias SerializedFileConvertible = SerializedFileReadable & Serialized
 // MARK: - Default Implementation
 
 public extension SerializedFileReadable {
-	init?(url: URL) {
-		do {
-			let data = try Data(contentsOf: url)
-			try self.init(data: data)
-		} catch {
-			return nil
-		}
+	init(url: URL) throws {
+		let data = try Data(contentsOf: url)
+		try self.init(data: data)
 	}
 }
 
 public extension SerializedFileWritable {
-	func save(to url: URL) {
+	func save(to url: URL) throws {
 		let fileManager = FileManager.default
-
-		do {
-			let folder = url.deletingLastPathComponent()
-			if !fileManager.fileExists(atPath: folder.path) {
-				try fileManager.createDirectory(at: folder, withIntermediateDirectories: true, attributes: nil)
-			}
-
-			if !fileManager.fileExists(atPath: url.path) {
-				fileManager.createFile(atPath: url.path, contents: nil, attributes: nil)
-			}
-
-			let data = try toData()
-			try data.write(to: url)
-		} catch {
-			print("Couldn't save file to \(url)", error)
+		let folder = url.deletingLastPathComponent()
+		if !fileManager.fileExists(atPath: folder.path) {
+			try fileManager.createDirectory(at: folder, withIntermediateDirectories: true, attributes: nil)
 		}
+
+		if !fileManager.fileExists(atPath: url.path) {
+			fileManager.createFile(atPath: url.path, contents: nil, attributes: nil)
+		}
+
+		let data = try toData()
+		try data.write(to: url)
 	}
 }
